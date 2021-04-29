02979ea7fff28e4d3e9feb710f9cadf2b6ca575a
Xavier Nunez <xaviernunez83@gmail.com>
2021-04-29 16:22:22 -0700

    finish structured-english

---
+++ is51test2.py
@@ -0,0 +1,8 @@
+""" 
+We are given a set of grades from a class final. We are to write a program that displays the number of final tests submitted, the class average, and the percentage above the class average.
+We will sum up the total of tests submitted. We will add the total scores together and divide them by the total number of tests submitted to calculate average. We will then take average test scores and 
+and see how many tests were above the average score. We will then calculate the percentage of tests that scored above the average. 
+
+
+
+"""
